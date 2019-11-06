60f463af746ddd0045aecab6f3297bf4c47993f3
Franz Heidl <franz@franzheidl.de>
2019-11-06 16:27:54 +0100

    Updates

--- publish.js
+++
@@ -1,9 +0,0 @@
-var ghpages = require('gh-pages');
-
-ghpages.publish('./', 'index.html', function(err) {
-    if (err) {
-        console.log(err);
-    } else {
-        console.log('published! 👍')
-    }
-});
